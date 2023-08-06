ade2db205a0f491a2e5a0292c1764f1fb1b10878
Eli <980883655@qq.com>
2023-08-06 11:51:46 +0800

    init: add rep to pro

--- src/hooks/useCardWidget.js
+++ src/hooks/useCardWidget.js
@@ -2,49 +2,12 @@ export function useCardWidget(props) {
   const title = <div>我是答案版大撒奥多多多多</div>
   const more = <span>more</span>
   const custom = <p>hello world . my name is sxx</p>
-  const content = <div>我都打算打打
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-    <p>helelelell家具家电</p>
-  </div>
+  const content = (
+    <div>
+      我都打算打打
+      <p>helelelell家具家电</p>
+    </div>
+  )
   return {
     title,
     content,
